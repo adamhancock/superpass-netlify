491f74f707c85ab2f4d03e1bcd72c052e1ef0a44
Adam Hancock <adammhancock@gmail.com>
2019-01-23 20:57:40 +0000

    init

---
+++ functions/simple.js
@@ -0,0 +1,29 @@
+'use strict';
+var Sentencer = require('sentencer');
+
+exports.handler = (event, context, callback) => {
+
+
+  var number = Math.floor(Math.random() * 101);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  var words = Sentencer.make("{{ adjective }}{{ noun }}");
+  var message = capitalizeFirstLetter(words) + number;
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
+    body: message,
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
