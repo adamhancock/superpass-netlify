4bf5a77ff66ed24715c3db7649d8651c5790d3d8
Adam Hancock <adammhancock@gmail.com>
2019-01-24 14:17:22 +0000

    Update simple.js

--- functions/simple.js
+++ functions/simple.js
@@ -1,24 +1,20 @@
 var Sentencer = require('sentencer');
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
 
 
-  var number = Math.floor(Math.random() * 101);
+  var number = await Math.floor(Math.random() * 101);
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  var words = Sentencer.make("{{ adjective }}{{ noun }}");
-  var message = capitalizeFirstLetter(words) + number;
+  var words = await Sentencer.make("{{ adjective }}{{ noun }}");
+  var message = await capitalizeFirstLetter(words) + number;
 
   const response = {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-    },
-    body: `${message}`,
+    body: message,
   };
 
   callback(null, response);
