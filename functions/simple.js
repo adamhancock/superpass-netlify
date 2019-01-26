31d0fa0e6c6067d4cfe66dc6197267e851b27994
Adam Hancock <adammhancock@gmail.com>
2019-01-26 15:03:37 +0000

    removed number

--- functions/simple.js
+++ functions/simple.js
@@ -1,23 +1,23 @@
 var Sentencer = require('sentencer');
 
-exports.handler = async (event, context, callback) => {
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+exports.handler = function (event, context, callback) {
 
 
-  var number = await Math.floor(Math.random() * 101);
+  // var number = Math.floor(Math.random() * 101);
 
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
 
-  var words = await Sentencer.make("{{ adjective }}{{ noun }}");
-  var message = await capitalizeFirstLetter(words) + number;
 
-  const response = {
-    statusCode: 200,
-    body: {"password": message},
-  };
+  var words = Sentencer.make("{{ adjective }}{{ noun }}");
+  var message = capitalizeFirstLetter(words);
 
-  callback(null, response);
+  callback(null, {
+    "statusCode": 200,
+    body: message,
+  });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
