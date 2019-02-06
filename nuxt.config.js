e2303a24f2601108019a7e1490d3a679a83051b3
Adam Hancock <adammhancock@gmail.com>
2019-02-06 09:21:33 +0000

    removed analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,10 +52,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/vuetify', {
-    src: '~plugins/googleanalytics.js',
-    ssr: false
-  }],
+  plugins: ['@/plugins/vuetify'],
 
   /*
    ** Nuxt.js modules
